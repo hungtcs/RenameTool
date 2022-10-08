a2a319a4776abe327744f52c86b1df365800e9e1
hungtcs <hungtcs@outlook.com>
2022-10-08 12:27:11 +0800

    Initial Commit

---
+++ RenameTool/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RenameTool
+//
+//  Created by 鸿则 on 2022/10/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RenameTool/RenameToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenameToolApp.swift
+//  RenameTool
+//
+//  Created by 鸿则 on 2022/10/8.
+//
+
+import SwiftUI
+
+@main
+struct RenameToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
